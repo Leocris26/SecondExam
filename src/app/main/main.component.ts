54be4777972dfc5f262120e1dc9f601942782186
Eun Kyu Choi <choi2nkyu@gmail.com>
2018-05-20 21:24:58 -0400

    SE-002: changed from link to button link

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onButtonClick1() {
+    this.router.navigateByUrl('/page1');
+  }
+
+  onButtonClick2() {
+    this.router.navigateByUrl('/page2');
   }
 
 }
